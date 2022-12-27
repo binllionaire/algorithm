ad0c24ebb6a0d0cbd1671b92768ab418f1a696ed
binllionaire <bug20474@gmail.com>
2022-12-27 19:24:09 +0900

    2022-12-27 : 좌표 정렬하기

---
+++ Sort/11650.py
@@ -0,0 +1,7 @@
+N = int(input())
+coordinate = []
+for i in range(N):
+    coordinate.append(list(map(int,input().split())))
+coordinate.sort()
+for i in range(N):
+    print(coordinate[i][0],coordinate[i][1])
